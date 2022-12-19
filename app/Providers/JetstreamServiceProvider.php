2b3491febc2e40e798b5d7010f0a7891addbcec9
Krystian Duma <git@krystian.duma.sh>
2022-12-19 01:29:42 +0100

    Update Jetstream Team Roles

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -49,19 +49,32 @@ class JetstreamServiceProvider extends ServiceProvider
      */
     protected function configurePermissions()
     {
-        Jetstream::defaultApiTokenPermissions(['read']);
-
         Jetstream::role('admin', 'Administrator', [
-            'create',
-            'read',
-            'update',
-            'delete',
+            'server:create', 'server:read', 'server:update', 'server:delete',
+            'printer:create', 'printer:read', 'printer:update', 'printer:delete',
+            'client:create', 'client:read', 'client:update', 'client:delete',
+            'job:create', 'job:read', 'job:update', 'job:delete',
+            'promise:create', 'promise:read', 'promise:update', 'promise:delete',
         ])->description('Administrator users can perform any action.');
 
         Jetstream::role('editor', 'Editor', [
-            'read',
-            'create',
-            'update',
+            'server:create', 'server:read', 'server:update',
+            'printer:create', 'printer:read', 'printer:update',
+            'client:create', 'client:read', 'client:update',
+            'job:create', 'job:read', 'job:update',
+            'promise:create', 'promise:read', 'promise:update',
         ])->description('Editor users have the ability to read, create, and update.');
+
+        Jetstream::role('viewer', 'Viewer', [
+            'server:read',
+            'printer:read',
+            'client:read',
+            'job:read',
+            'promise:read',
+        ])->description('Viewer users have the ability only to read.');
+
+        // Jetstream::defaultApiTokenPermissions([]);
+
+        // Jetstream::permissions([]);
     }
 }
