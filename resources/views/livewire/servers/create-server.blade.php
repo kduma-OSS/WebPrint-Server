daa10d8caa150a6fa422f572f74b234b123bc4e4
Krystian Duma <git@krystian.duma.sh>
2022-12-26 03:05:19 +0100

    Fixes to #14 & #15

--- resources/views/livewire/servers/create-server.blade.php
+++ resources/views/livewire/servers/create-server.blade.php
@@ -12,11 +12,7 @@
         <!-- Name -->
         <div class="col-span-6 sm:col-span-4">
             <x-jet-label for="name" value="{{ __('print-servers.name_label') }}" />
-            @can('update', [\App\Settings\GeneralSettings::class, 'name'])
-                <x-jet-input id="name" type="text" class="mt-1 block w-full" wire:model.defer="name" />
-            @else
-                <x-jet-input id="name" type="text" class="mt-1 block w-full" wire:model.defer="name" disabled />
-            @endcan
+            <x-jet-input id="name" type="text" class="mt-1 block w-full" wire:model.defer="name" />
             <x-jet-input-error for="name" class="mt-2" />
         </div>
     </x-slot>
